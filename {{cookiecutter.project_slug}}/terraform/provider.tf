77126da8f368c5e2ef6e27231ccfba5ae8df200e
Arun Ramachandran <100121618+thecloudtechin@users.noreply.github.com>
2023-09-12 16:28:02 +0530

    Update provider.tf

--- {{cookiecutter.project_slug}}/terraform/provider.tf
+++ {{cookiecutter.project_slug}}/terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     access_key = "{{cookiecutter.access_key}}"
     secret_key = "{{cookiecutter.secret_key}}"
-    region = "{{cookiecutter.region}}"
+    region = "{{cookiecutter.aws_region}}"
   
 }
